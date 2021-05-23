dd0d1018bd55d0fb136b137edd2d18e0c65b1b1d
unknown <rdg.abrantes@gmail.com>
2021-05-23 01:43:40 -0300

    configuracao ec2-linux

--- ec2.tf
+++ ec2.tf
@@ -4,8 +4,19 @@ resource "aws_instance" "dev-windows" {
     ami = "${var.ec2-ami["windows"]}"
     instance_type = "${var.ec2-config-family}"
     key_name = "${var.aws-account}"
+    vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
     tags = {
         Name = "ec2-windows-server-${var.ec2-config-family}"
     }    
     
+}
+
+resource "aws_instance" "dev-linux" {
+      ami = "${var.ec2-ami["linux"]}"
+      instance_type = "${var.ec2-config-family}"
+      key_name = "${var.aws-account}"
+      vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
+      tags = {
+        Name = "ec2-linux-${var.ec2-config-family}"
+      }
 }
